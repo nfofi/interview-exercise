6d1cab1f3151a26871e2b9cc90aca9c584c41ee9
Nicholas <nick.fofi@gmail.com>
2017-10-02 18:13:20 -0500

    "Database" interface and implementation

---
+++ Interview-Exercise/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Interview_Exercise
+{
+    class Database : IDatabase
+    {
+        public string getDataPath()
+        {
+            string dataPath = Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),"Interview-Exercise");
+
+            return dataPath;
+        }
+    }
+}
---
+++ Interview-Exercise/IDatabase.cs
@@ -0,0 +1,11 @@
+﻿namespace Interview_Exercise
+{
+    interface IDatabase
+    {
+        /// <summary>
+        /// Get the data path for the repo
+        /// </summary>
+        /// <returns></returns>
+        string getDataPath();
+    }
+}
