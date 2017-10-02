50fe9803ab3324634265df805efcedbea3981c62
Nicholas <nick.fofi@gmail.com>
2017-10-02 18:12:25 -0500

    Creating Country class with appropriate properties

--- Interview-Exercise/Country.cs
+++ Interview-Exercise/Country.cs
@@ -2,6 +2,28 @@
 {
     public class Country
     {
-        
+        private string code;
+        public string Code
+        {
+            get { return code; }
+            set { if (isUnique() && isValid())
+                    code = value.ToUpper();
+                }
+        }
+
+        private string name;
+        public string Name { get; set; }
+
+        private bool isUnique()
+        {
+            //method to determine if code is unique
+            return true;
+        }
+
+        private bool isValid()
+        {
+            //method to determine if code a 3-character string and is not User-Assigned
+            return true;
+        }
     }
 }
