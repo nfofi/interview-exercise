504dc0237b44e49ccbcfe1392e151a382fd06aa8
Nicholas <nick.fofi@gmail.com>
2017-10-03 10:18:06 -0500

    First unit test for repo class.

---
+++ Interview-Exercise-Tests/UnitTests/CountryCodeRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Interview_Exercise;
+
+namespace Interview_Exercise_Tests.UnitTests
+{
+    public class CountryCodeRepositoryTest
+    {
+        public class WhenAddingANewCountryToTheRepo: TestFixtureBase
+        {
+            private IDatabase _database;
+            private Country _country;
+
+            protected override void Arrange()
+            {
+                _country = new Country("USA", "United States");
+
+                _database = Mock<IDatabase>();
+                _database.Expect(x => x.Insert(_country));
+            }
+
+            protected override void Act()
+            {
+                var testRepo = new CountryCodeRepository(_database);
+                testRepo.Add(_country);
+            }
+
+            [Test]
+            public void Should_have_used_the_dependency()
+            {
+                _database.AssertWasCalled(x => x.Insert(_country), y => y.Repeat.Once());
+            }
+        }
+    }
+}
